c5c525b15444724778486d293a903998827da66e
amandagm77 <amanda.gmb@outlook.com>
2026-06-03 13:40:13 -0600

    Building main App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import { useState } from 'react'
-import './App.css'
-import StudentForm from './components/StudentForm'
-import StudentList from './components/StudentList'
-import StudentCard from './components/StudentCard'
+import { useState } from "react";
+import "./App.css";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
 
 function App() {
-    return 
+  const [students, setStudents] = useState([
+    {
+      id: 1,
+      name: "Amanda",
+      course: "Software Engineering",
+    },
+  ]);
+
+  function addStudent(student) {
+    setStudents([...students, student]);
+  }
+
+  function deleteStudent(id) {
+    const updatedStudents = students.filter(
+      (student) => student.id !== id
+    );
+
+    setStudents(updatedStudents);
+  }
+
+  return (
+    <div className="app">
+      <h1>Student Management App</h1>
+
+      <StudentForm addStudent={addStudent} />
+
+      <StudentList
+        students={students}
+        deleteStudent={deleteStudent}
+      />
+    </div>
+  );
 }
+
 export default App;

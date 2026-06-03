a5f95ead622fcb8ddbae349ffa10be24789137a0
amandagm77 <amanda.gmb@outlook.com>
2026-06-03 13:35:39 -0600

    Fixed StudentCard component

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,47 +1,14 @@
-import { useState } from "react";
-
-function StudentForm({ addStudent }) {
-  const [name, setName] = useState("");
-  const [course, setCourse] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!name || !course) return;
-
-    const newStudent = {
-      id: Date.now(),
-      name,
-      course,
-    };
-
-    addStudent(newStudent);
-
-    setName("");
-    setCourse("");
-  }
-
+function StudentCard({ name, course, onDelete, id }) {
   return (
-    <form onSubmit={handleSubmit} className="student-form">
-      <input
-        type="text"
-        placeholder="Student Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+    <div className="student-card">
+      <h2>{name}</h2>
+      <p>{course}</p>
 
-      <input
-        type="text"
-        placeholder="Course"
-        value={course}
-        onChange={(e) => setCourse(e.target.value)}
-      />
-
-      <button type="submit">
-        Add Student
+      <button onClick={() => onDelete(id)}>
+        Delete
       </button>
-    </form>
+    </div>
   );
 }
 
-export default StudentForm;
+export default StudentCard;

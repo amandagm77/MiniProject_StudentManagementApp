337fbbf8a3463ee533f72a1e465425a3ff3772c2
amandagm77 <amanda.gmb@outlook.com>
2026-06-03 13:13:18 -0600

    First Commit, Project Structure Skeleton

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+import StudentForm from './components/StudentForm'
+import StudentList from './components/StudentList'
+import StudentCard from './components/StudentCard'
+
+function App() {
+    return 
+}
+export default App;
